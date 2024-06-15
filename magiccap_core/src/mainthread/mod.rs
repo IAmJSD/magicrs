3591555cde76d910ce4dbd5b9c7517482f5e1515
Astrid Gealer <astrid@gealer.email>
2024-06-15 14:53:00 +0100

    untested but hopefully this works

--- magiccap_core/src/mainthread/mod.rs
+++ magiccap_core/src/mainthread/mod.rs
@@ -31,6 +31,10 @@ where
     app().main_thread_writer.send(Box::new(handler));
 }
 
+// Include the one usage channel on Linux.
+#[cfg(target_os = "linux")]
+mod one_usage_channel;
+
 // Handles the main thread synchronously on Linux.
 #[cfg(target_os = "linux")]
 pub fn main_thread_sync<F, T>(handler: F) -> T
@@ -38,22 +42,22 @@ where
     F: Send + FnOnce() -> T, T: Send
 {
     use crate::linux_shared::app;
-    use std::sync::mpsc::channel;
+    use crate::mainthread::one_usage_channel::channel;
 
     // Box the handler into a raw pointer. This is to get around some Rust rules
     // that are not overly useful here.
     let handler_ptr = Box::into_raw(Box::new(handler)) as usize;
 
     // Defines the function handler.
-    let (tx, rx) = channel();
+    let (mut sender, reciever) = channel();
     app().main_thread_writer.send(Box::new(move || {
         let handler: Box<F> = unsafe { Box::from_raw(handler_ptr as *mut F) };
         let res = handler();
-        tx.send(Box::into_raw(Box::new(res)) as usize).unwrap();
+        sender.send(Box::into_raw(Box::new(res)) as usize);
     }));
 
     // Wait for the result and then return it.
-    let res_ptr = rx.recv().unwrap();
+    let res_ptr = reciever.wait();
     unsafe {
         let res = Box::from_raw(res_ptr as *mut T);
         *res
