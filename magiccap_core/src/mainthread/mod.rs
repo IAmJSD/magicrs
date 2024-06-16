dca7a1451f2cafd62f6dc04a7a4b70b19096b3b1
Astrid Gealer <astrid@gealer.email>
2024-06-16 19:15:04 +0100

    ok, rust wins

--- magiccap_core/src/mainthread/mod.rs
+++ magiccap_core/src/mainthread/mod.rs
@@ -31,32 +31,28 @@ where
     idle_add_once(handler);
 }
 
-// Include the one usage channel module on Linux.
-#[cfg(target_os = "linux")]
-mod one_usage_channel;
-
 // Handles the main thread synchronously on Linux.
 #[cfg(target_os = "linux")]
 pub fn main_thread_sync<F, T>(handler: F) -> T
 where
     F: Send + FnOnce() -> T, T: Send
 {
-    use crate::mainthread::one_usage_channel::channel;
+    use std::sync::mpsc::channel;
 
     // Box the handler into a raw pointer. This is to get around some Rust rules
     // that are not overly useful here.
     let handler_ptr = Box::into_raw(Box::new(handler)) as usize;
 
     // Defines the function handler.
-    let (mut sender, reciever) = channel();
+    let (sender, reciever) = channel();
     main_thread_async(move || {
         let handler: Box<F> = unsafe { Box::from_raw(handler_ptr as *mut F) };
         let res = handler();
-        sender.send(Box::into_raw(Box::new(res)) as usize);
+        sender.send(Box::into_raw(Box::new(res)) as usize).unwrap();
     });
 
     // Wait for the result and then return it.
-    let res_ptr = reciever.wait();
+    let res_ptr = reciever.recv().unwrap();
     unsafe {
         let res = Box::from_raw(res_ptr as *mut T);
         *res
