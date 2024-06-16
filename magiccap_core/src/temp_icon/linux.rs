e404c87bc9fef0ff2b0f5c7f9b3dfc3e8be2c9af
Astrid Gealer <astrid@gealer.email>
2024-06-16 15:34:05 +0100

    add IconHandler

--- magiccap_core/src/temp_icon/linux.rs
+++ magiccap_core/src/temp_icon/linux.rs
@@ -1,7 +1,10 @@
 use muda::MenuEvent;
 use tray_icon::{TrayIcon, TrayIconBuilder, menu::{MenuItem, Menu}};
 use crate::linux_shared::FakeSend;
-use std::{sync::atomic::{AtomicBool, Ordering}, thread, io::Read};
+use std::{
+    io::{Read, BufRead, Write}, process,
+    sync::{atomic::{AtomicBool, Ordering}, Mutex}, thread,
+};
 use crate::temp_icon::shared::{STOP_ICON, COG_ICON};
 
 // Defines the slot for the temporary icon demon sender.
@@ -22,6 +25,9 @@ fn tray_updates() {
 
     // Update the icon.
     sent.value.set_icon(Some(COG_ICON.clone())).unwrap();
+
+    // Log that we are in the processing stage.
+    println!("MAGICCAP_PROCESSING");
 }
 
 // Defines the function to stop recording on Linux.
@@ -79,4 +85,74 @@ pub fn icond() {
 
     // Call gtk::main.
     gtk::main();
+}
+
+// Defines the icon handler. When the icon is dropped, it will be removed from the tray.
+pub struct IconHandler {
+    process: Mutex<process::Child>,
+}
+
+// Build a child process of ourselves but with the MAGICCAP_INTERNAL_TEMP_ICON env var set.
+fn create_icond(cb: Box<dyn FnOnce() + Send>) -> process::Child {
+    // Get the current executable path.
+    let path = std::env::current_exe().unwrap();
+
+    // Create the process.
+    let mut proc = process::Command::new(path)
+        .env("MAGICCAP_INTERNAL_TEMP_ICON", "1")
+        .stdout(process::Stdio::piped())
+        .stdin(process::Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    // Spawn a thread to handle stdout.
+    let mut stdout = proc.stdout.take().unwrap();
+    thread::spawn(move || {
+        loop {
+            // Read to a new line and store it in a buffer.
+            let mut buffer = String::new();
+            let buffer = match std::io::BufReader::new(&mut stdout).read_line(&mut buffer) {
+                Ok(_) => buffer,
+                Err(_) => return,
+            };
+
+            // If the buffer contains "MAGICCAP_PROCESSING", call the callback and return.
+            if buffer.contains("MAGICCAP_PROCESSING") {
+                cb();
+                return;
+            }
+        }
+    });
+
+    // Return the process.
+    proc
+}
+
+// Defines the icon handler implementation.
+impl IconHandler {
+    // Create a new version of the icon handler. Note that the stop callback
+    // will NOT be ran in the main thread in all cases.
+    pub fn new(stop_callback: Box<dyn FnOnce() + Send>) -> Self {
+        Self {
+            process: Mutex::new(create_icond(stop_callback)),
+        }
+    }
+
+    // Handles stopping the recording and putting the icon into processing mode.
+    // Note that this will also invoke the stop callback.
+    pub fn stop_recording(&self) {
+        // Get the process.
+        let mut proc = self.process.lock().unwrap();
+
+        // Write 's' to the stdin.
+        let _ = proc.stdin.as_mut().unwrap().write(b"s");
+    }
+}
+
+// Handle dropping the icon handler. This will remove the icon from the tray.
+impl Drop for IconHandler {
+    fn drop(&mut self) {
+        let mut proc = self.process.lock().unwrap();
+        let _ = proc.kill();
+    }
 }
--- magiccap_core/src/temp_icon/mod.rs
+++ magiccap_core/src/temp_icon/mod.rs
@@ -7,3 +7,9 @@ mod other;
 // On Linux, re-export the icond function.
 #[cfg(target_os = "linux")]
 pub use linux::icond;
+
+// Re-export the icon handler.
+#[cfg(target_os = "linux")]
+pub use linux::IconHandler;
+#[cfg(not(target_os = "linux"))]
+pub use other::IconHandler;
