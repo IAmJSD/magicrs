304e44f69a83038f55df7472b8471faad49f75ea
Astrid Gealer <astrid@gealer.email>
2024-06-28 01:40:49 +0100

    fix trans-parency

--- magiccap_core/src/region_selector/magnifier.rs
+++ magiccap_core/src/region_selector/magnifier.rs
@@ -1,5 +1,14 @@
 use super::engine::RegionSelectorContext;
 
+// Defines the gap between the cursor and the magnifier.
+const GAP: i32 = 15;
+
+// Defines the size of the plucked content.
+const PLUCKED_SIZE: i32 = 50;
+
+// Defines the size of the magnifier.
+const MAGNIFIER_SIZE: i32 = 125;
+
 // Renders the magnifier.
 pub unsafe fn render_magnifier(
     ctx: &mut RegionSelectorContext, index: usize, cursor_x: i32, cursor_y: i32,
@@ -7,21 +16,34 @@ pub unsafe fn render_magnifier(
 ) {
     let (mut x, mut y) = (cursor_x, cursor_y);
 
-    if x + 10 + 100 > width {
+    if x + GAP + MAGNIFIER_SIZE > width {
         // Make the magnifier render to the left if otherwise it would go off the screen.
-        x -= 10 + 100;
+        x -= GAP + MAGNIFIER_SIZE;
     } else {
         // Add a gap between the cursor and the magnifier.
-        x += 10;
+        x += GAP;
     }
 
-    if y + 10 + 100 > height {
+    if y + GAP + MAGNIFIER_SIZE > height {
         // Make the magnifier render above the cursor if otherwise it would go off the screen.
-        y -= 10 + 100;
+        y -= GAP + MAGNIFIER_SIZE;
     } else {
         // Add a gap between the cursor and the magnifier.
-        y += 10;
+        y += GAP;
     }
+
+    // Defines the dest X/Y.
+    let x0 = x;
+    let y0 = height - y - MAGNIFIER_SIZE;
+    let x1 = x + MAGNIFIER_SIZE;
+    let y1 = height - y;
+
+    // Draw a black box where the magnifier will be. This involves clearing the specific area.
+    gl::Scissor(x0, y0, MAGNIFIER_SIZE, MAGNIFIER_SIZE);
+    gl::Enable(gl::SCISSOR_TEST);
+    gl::ClearColor(0.0, 0.0, 0.0, 1.0);
+    gl::Clear(gl::COLOR_BUFFER_BIT);
+    gl::Disable(gl::SCISSOR_TEST);
 
     // Bind the framebuffer to the texture used for the un-dark content.
     gl::FramebufferTexture2D(
@@ -31,8 +53,8 @@ pub unsafe fn render_magnifier(
 
     // Blit a stretched version of the content around the cursor to the magnifier.
     gl::BlitFramebuffer(
-        cursor_x - 10, cursor_y + 10, cursor_x + 10, cursor_y - 10,
-        x, height - y - 100, x + 100, height - y,
+        cursor_x - PLUCKED_SIZE, cursor_y + PLUCKED_SIZE, cursor_x + PLUCKED_SIZE,
+        cursor_y - PLUCKED_SIZE, x0, y0, x1, y1,
         gl::COLOR_BUFFER_BIT, gl::NEAREST,
     );
 }
