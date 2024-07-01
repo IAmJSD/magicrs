10c6030253f6b5c0e9aa3b92eb69690cf584e319
Astrid Gealer <astrid@gealer.email>
2024-07-01 23:13:01 +0100

    Revert "add acquire_application_lock"
    This reverts commit 81bf3c92d4553d96697a11313f272a9670026a0e.

--- magiccap_core/src/lib.rs
+++ magiccap_core/src/lib.rs
@@ -1,4 +1,3 @@
-mod application_lock;
 mod capture;
 mod clipboard_actions;
 mod config;
@@ -14,10 +13,6 @@ mod utils;
 mod region_selector;
 mod temp_icon;
 
-use application_lock::acquire_application_lock;
-use config::open_config;
-use mainthread::main_thread_async;
-
 #[cfg(target_os = "macos")]
 mod macos_delegate;
 #[cfg(target_os = "macos")]
@@ -26,21 +21,13 @@ mod macos;
 #[cfg(target_os = "linux")]
 mod linux_shared;
 
+#[cfg(target_os = "macos")]
 #[no_mangle]
-pub unsafe extern "C" fn application_init() {
-    // Handle the single instance lock.
-    acquire_application_lock(|| {
-        main_thread_async(|| open_config());
-    });
+pub unsafe extern "C" fn application_init() { macos_delegate::application_init() }
 
-    // Call the application init logic.
-    #[cfg(target_os = "macos")]
-    macos_delegate::application_init();
-    #[cfg(target_os = "linux")]
-    linux_shared::application_init();
-    #[cfg(not(any(target_os = "macos", target_os = "linux")))]
-    compile_error!("Unsupported OS");
-}
+#[cfg(target_os = "linux")]
+#[no_mangle]
+pub unsafe extern "C" fn application_init() { linux_shared::application_init() }
 
 #[no_mangle]
 pub unsafe extern "C" fn application_reload() {
