c0138e1fae2e23e1508d713c20901de39880e72e
Astrid Gealer <astrid@gealer.email>
2024-07-11 18:11:49 +0100

    a new rabbit hole has started

--- magiccap_core/src/uploaders/s3.rs
+++ magiccap_core/src/uploaders/s3.rs
@@ -1,10 +1,80 @@
-use std::{collections::HashMap, time::SystemTime};
+use std::collections::HashMap;
+use ureq::Middleware;
 use super::{mime, ConfigOption, Uploader};
-use aws_sigv4::{
-    http_request::{SignableBody, SignableRequest, sign, SigningParams as HTTPSigningParams, SigningSettings},
-    sign::v4::SigningParams,
-};
-use aws_credential_types::Credentials;
+
+struct S3Signing {
+    access_key_id: String,
+    secret_access_key: String,
+    body_hash: String,
+    region: String,
+}
+
+fn s3_url_encode(content: &str) -> String {
+    let mut encoded = String::new();
+    for c in content.chars() {
+        match c {
+            'A'..='Z' | 'a'..='z' | '0'..='9' | '_' | '-' | '~' | '.' | '/' => {
+                encoded.push(c);
+            },
+            _ => {
+                encoded.push('%');
+                encoded.push_str(&format!("{:02X}", c as u8));
+            },
+        }
+    }
+    encoded
+}
+
+impl Middleware for S3Signing {
+    fn handle(&self, request: ureq::Request, next: ureq::MiddlewareNext) -> Result<ureq::Response, ureq::Error> {
+        // Add the start method/URI.
+        let url = request.request_url().unwrap();
+        let canonical_uri = url.path();
+        let mut string_pointers = Vec::new();
+        string_pointers.push(request.method());
+        string_pointers.push(&s3_url_encode(canonical_uri));
+
+        // Add the query parameters.
+        let q = url.query_pairs();
+        q.sort_by(|a, b| a.0.cmp(&b.0));
+        let query = q.into_iter().map(|(k, v)| {
+            s3_url_encode(k) + "=" + &s3_url_encode(v)
+        }).collect::<Vec<_>>().join("&");
+        if !query.is_empty() {
+            string_pointers.push(&query);
+        }
+
+        // Add the headers.
+        let mut header_names = request.header_names().into_iter().map(|h| {
+            h.to_lowercase()
+        }).collect::<Vec<_>>();
+        header_names.sort();
+        for header_name in header_names {
+            let header_value = request.header(&header_name).unwrap();
+            let header = header_name + ":" + header_value;
+            string_pointers.push(&header);
+        }
+        let signed_headers = header_names.join(";");
+        string_pointers.push(&signed_headers);
+
+        // Add the body to the string pointers.
+        string_pointers.push(&self.body_hash);
+
+        // Create the canonical request.
+        let everything_nl_joined = string_pointers.join("\n");
+        let canonical_request = sha256::digest(everything_nl_joined.as_bytes());
+
+        // Create the string to sign.
+        let iso_time = chrono::Utc::now().format("%Y%m%dT%H%M%SZ").to_string();
+        let string_to_sign = "AWS4-HMAC-SHA256\n".to_string() +
+            &iso_time + "\n" +
+            &iso_time[0..8] + &format!("/{}/s3/aws4_request\n", s3_url_encode(&self.region)) +
+            &canonical_request;
+
+        // Create the signing key.
+        // TODO
+    }
+}
 
 fn s3_support_upload(
     filename: &str, config: HashMap<String, serde_json::Value>,
@@ -60,83 +130,20 @@ fn s3_support_upload(
         return Err(format!("Failed to read the body: {}", e));
     }
 
-    // Build the headers.
-    let acl = match config.get("acl") {
-        Some(acl) => acl.as_str().unwrap(),
-        None => "public-read",
-    };
-    let len_s = body.len().to_string();
-    let mime_s = mime.to_string();
-    let mut headers = vec![
-        ("content-length", len_s.as_str()),
-        ("content-type", mime_s.as_str()),
-        ("x-amz-acl", acl),
-        ("cache-control", "max-age=31536000"),
-        ("accept-encoding", "gzip"),
-        ("user-agent", "MagicCap"),
-    ];
-    // TODO: figure out why the signature is invalid
-
-    // Perform the signing.
-    let url_pre_sign = url.to_string();
-    let signable_request = match SignableRequest::new(
-        "PUT", url_pre_sign.as_str(), headers.iter().cloned(), SignableBody::Bytes(&body), 
-    ) {
-        Ok(r) => r,
-        Err(e) => return Err(format!("Failed to create the signable request: {}", e)),
-    };
-    let identity = Credentials::new(
-        config.get("access_key_id").unwrap().as_str().unwrap(),
-        config.get("secret_access_key").unwrap().as_str().unwrap(),
-        None, None, "hardcoded-credentials",
-    ).into();
-    let region = match config.get("region") {
-        Some(r) => r.as_str().unwrap(),
-        None => "us-east-1",
-    };
-    let signing_params = match SigningParams::builder().
-        region(region).
-        identity(&identity).
-        name("s3").
-        time(SystemTime::now()).
-        settings(SigningSettings::default()).
-        build()
-    {
-        Ok(p) => p,
-        Err(e) => return Err(format!("Failed to create the signing params: {}", e)),
-    };
-    (
-        "s3", region,
-        config.get("access_key_id").unwrap().as_str().unwrap(),
-        config.get("secret_access_key").unwrap().as_str().unwrap(),
-    );
-    let signing_output = match sign(
-        signable_request, &HTTPSigningParams::from(signing_params),
-    ) {
-        Ok(r) => r,
-        Err(e) => return Err(format!("Failed to sign the request: {}", e)),
-    };
-
-    // Add the headers to the request.
-    let si_output = signing_output.output();
-    headers.extend(si_output.headers());
-    let mut query = "".to_string();
-    let new_query = si_output.params();
-    if !new_query.is_empty() {
-        for (k, v) in new_query {
-            query += k;
-            query += "=";
-            query += v.to_string().as_str();
-            query += "&";
-        }
-        url.set_query(Some(query.as_str())).unwrap();
-    }
+    let agent = ureq::builder().middleware(S3Signing {
+        access_key_id: config.get("access_key_id").unwrap().to_string(),
+        secret_access_key: config.get("secret_access_key").unwrap().to_string(),
+        body_hash: sha256::digest(&body),
+        region: config.get("region").unwrap_or(
+            &serde_json::Value::String("us-east-1".to_string())).as_str().unwrap().to_string(),
+    }).build();
+    let req = agent.put(url.to_string().as_str()).
+        set("Content-Type", &mime.to_string()).
+        set("x-amz-acl", config.get("acl").unwrap_or(
+            &serde_json::Value::String("public-read".to_string())).as_str().unwrap()).
+        set("User-Agent", "MagicCap");
 
     // Perform the request.
-    let mut req = ureq::put(url.to_string().as_str());
-    for (k, v) in &headers {
-        req = req.set(k, v);
-    }
     if let Err(e) = req.send_bytes(&body) {
         match e {
             ureq::Error::Status(code, response) => {
