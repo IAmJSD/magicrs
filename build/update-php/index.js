4f6dbb1032354af5fdbe34a2d6df624d4b48fe7d
Astrid Gealer <astrid@gealer.email>
2024-07-19 01:50:16 +0100

    fix the artifacts update script

--- build/update-php/index.js
+++ build/update-php/index.js
@@ -6,74 +6,137 @@ const { writeFileSync } = require("fs");
 const AdmZip = require("adm-zip");
 const crypto = require("crypto");
 
-const PREFIX = /^php-(\d+\.\d+)/;
-
-async function processArtifact(artifact, runId) {
-    // Build the download URL.
-    const downloadUrl = "https://github.com/static-php/static-php-cli-hosted/actions/runs/" + runId + "/artifacts/" + artifact.id;
-
-    // Get either 'php' or 'php.exe' from the ZIP.
+function hashZip(data) {
+    // Load the zip file.
     const zip = new AdmZip(data);
-    const zipEntries = zip.getEntries();
-    const entry = zipEntries.find(entry => entry.entryName.endsWith("php") || entry.entryName.endsWith("php.exe"));
-    if (!entry) {
-        throw new Error("Could not find PHP binary in artifact.");
-    }
+    const zipEntries = zip.getEntries().sort((a, b) => {
+        return a.entryName.localeCompare(b.entryName);
+    });
 
-    // Extract the PHP binary as a buffer.
-    const buffer = entry.getData();
+    // Map into an array of [name, hash].
+    const hashes = zipEntries.map(entry => {
+        const hash = crypto.createHash("sha256");
+        hash.update(entry.getData());
+        return [entry.entryName, hash.digest("hex")];
+    });
 
-    // sha256 the buffer.
+    // Create a hash of the hashes.
     const hash = crypto.createHash("sha256");
-    hash.update(buffer);
-    const sha256 = hash.digest("hex");
-
-    // Return the URL and hash.
-    return {
-        url: artifact.archive_download_url,
-        hash: sha256,
-    };
+    hash.update(JSON.stringify(hashes));
+    return hash.digest("hex");
 }
 
 async function main() {
-    // Get all the workflow runs.
-    const { data } = await get("https://api.github.com/repos/static-php/static-php-cli-hosted/actions/workflows/77464426/runs", {
-        responseType: "json",
-    });
+    // Get the releases from the GitHub releases.
+    const { data } = await get(
+        "https://api.github.com/repos/WebScaleSoftwareLtd/magiccap-php-binaries/releases",
+    );
+    if (!Array.isArray(data) || data.length === 0) {
+        throw new Error("No releases found.");
+    }
+    const release = data[0];
 
-    // Find the first successful run.
-    const successfulRun = data.workflow_runs.find(run => run.conclusion === "success");
-
-    // Visit the artifacts_url of the successful run.
-    const artifacts = (await get(successfulRun.artifacts_url, {
-        responseType: "json",
-    })).data.artifacts;
-
-    // Reverse the artifacts and then break when we find the next PHP version.
-    const latestVersionArtifacts = [];
-    const lastVersion = artifacts[artifacts.length - 1].name.match(PREFIX)[1];
-    for (const artifact of artifacts.reverse()) {
-        const version = artifact.name.match(PREFIX)[1];
-        if (version !== lastVersion) {
+    // Get the PHP Windows releases.
+    const { data: phpReleases } = await get(
+        "https://windows.php.net/downloads/releases/releases.json",
+    );
+    let phpRelease = null;
+    for (const releaseKey of Object.keys(phpReleases)) {
+        // Check if the name is with the same version.
+        if (release.name.startsWith(releaseKey)) {
+            phpRelease = phpReleases[releaseKey];
             break;
         }
-        latestVersionArtifacts.push(artifact);
+    }
+    if (!phpRelease) {
+        throw new Error("No PHP Windows release found.");
     }
 
-    // Defines the map and then handle the artifacts.
-    const artifactMap = {};
-    const promises = artifacts.map(artifact => (async () => {
-        const res = await processArtifact(artifact);
-        artifactMap[artifact.name] = res;
+    // Get the releases from GitHub.
+    const releases = {};
+    const promises = [];
+    for (const asset of release.assets) {
+        promises.push((async () => {
+            // Get the name and URL of the asset.
+            const name = asset.name;
+            const url = asset.browser_download_url;
+
+            // Get the asset URL.
+            const { data } = await get(url, {
+                responseType: "arraybuffer",
+            });
+
+            // Create a hash of the asset.
+            const hash = crypto.createHash("sha256");
+            hash.update(data);
+            const sha256 = hash.digest("hex");
+
+            // Add the asset to the releases.
+            releases[name] = {
+                url,
+                sha256,
+                zip: false,
+            };
+        })());
+    }
+
+    // Get the nts-vc15-x64 version.
+    const ntsVs16X64 = phpRelease["nts-vs16-x64"];
+    if (!ntsVs16X64) {
+        throw new Error("No nts-vc16-x64 version found.");
+    }
+
+    // Check if there is a arm64 version.
+    const arm64 = phpRelease["nts-vs16-arm64"];
+
+    // Handle the promise to get the x64 version.
+    promises.push((async () => {
+        // Get the asset URL.
+        const url = `https://windows.php.net/downloads/releases/${ntsVs16X64.zip.path}`;
+        const { data } = await get(url, {
+            responseType: "arraybuffer",
+        });
+
+        // Create the info blob.
+        const info = {
+            url,
+            sha256: hashZip(data),
+            zip: true,
+        };
+
+        // Add the asset to the releases.
+        releases["php-windows-x86_64"] = info;
+        if (!arm64) {
+            // There is no ARM build meaning that ARM windows systems will need to emulate x64.
+            releases["php-windows-arm64"] = info;
+        }
     })());
-    await Promise.all(promises);
+
+    // Handle the promise to get the arm64 version.
+    if (arm64) {
+        promises.push((async () => {
+            // Get the asset URL.
+            const url = `https://windows.php.net/downloads/releases/${arm64.zip.path}`;
+            const { data } = await get(url, {
+                responseType: "arraybuffer",
+            });
+
+            // Add the asset to the releases.
+            releases["php-windows-arm64"] = {
+                url,
+                sha256: hashZip(data),
+                zip: true,
+            };
+        })());
+    }
 
     // Write the artifact map to a file.
+    await Promise.all(promises);
     const path = join(
         __dirname, "..", "..", "php_artifacts.json",
     );
-    writeFileSync(path, JSON.stringify(artifactMap, null, 4));
-    console.log(`PHP version updated to ${lastVersion}`);
+    writeFileSync(path, JSON.stringify(releases, null, 4) + "\n");
+    console.log(`PHP version updated to ${release.name}`);
 }
 
 main().catch(e => {
